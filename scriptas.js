710fc974eeabccaf46815d0e27be0a8508776ca4
Ina <samanaviciute@gmail.com>
2025-01-11 14:15:17 +0200

    Created project

---
+++ scriptas.js
@@ -0,0 +1,48 @@
+/* 
+1. Parašykite f-ją, kuriai perduodamas vienas parametras, ir ji nustatytų ar skaičius yra lyginis ar ne. 
+Išvedamo atsakymo pvz. "X skaičius yra lyginis" /  "X skaičius yra nelyginis" 
+*/
+
+
+/* 
+2. Parašykite f-ją, kuriai perduodami du parametrai (skaičius ir daliklis), paskaičiuotumėte likutį dalinant.  
+Išvedamo atsakymo pvz.: "Dalinant X iš Y, liekana yra Z" 
+*/
+
+/*
+3. Parašykite f-ją, kuriai perduodami du parametrai, paskaičiuotų dviejų skaičių vidurkį 
+Išvedamo atsakymo pvz.: "Skaičių X ir Y vidurkis Z"
+*/
+
+/*
+4. Laikrodis rodo laiką h, m, s. Parašykite programą, kuri apskaičiuotų kiek sekundžių prabėgo po vidurnakčio. (perduodami 
+trys parametrai) 
+Išvedamo atsakymo pvz.: "Po vidurnakčio prabėgo XXX sekundžių"
+*/
+
+/*
+5. Parašykite programą kuri patikrintų ar skaičius  dalinasi iš 3, 5 ar iš abiejų tai yra ir iš 3 ir iš 5.  
+Jei dalinasi iš 3 be liekanos atspausdinam „Labas“,  jei dalinasi iš 5 be liekanos atspausdinam „Rytas“,  
+jei dalinasi ir iš 3 ir iš 5 atspausdinam „Labas rytas“, jei nesidalina nei iš 3 nei iš 5 išvedam paprasčiausiai skaičių. 
+Išvedamo atsakymo pvz.: 3 => „Labas“ arba 5 => „Rytas“  arba 15 => „Labas Rytas“ arba 7 => 7
+*/
+
+/*
+6. Jonas ir Petras dalyvavo šaškių turnyre. Jonas surinko n taškų o Petras m. Nustatykite kuris iš dalyvių surinko daugiau 
+taškų turnyre.  
+Išvedamo atsakymo pvz.: „Laimėtojo vardas : xxxxx“
+*/
+
+/*
+7. Žinomi kambario matmenys ilgis ir plotis. Reikia apskaičiuoti, kokią pinigų sumą reiks mokėti už plyteles, skirtas kambario 
+grindims kloti, jei plytelių vieno kvadrato kaina yra 23 eurai. Plytelių reikia pirkti 5 proc. daugiau galimiems nuostoliams padengti. 
+Plytelės supakuotos po 1.5m2 ir parduotuvės darbuotojai nesutinka ardyti pakuočių. Galima pirkti tik pilnas pakuotes.  
+Išvedamo atsakymo pvz.: „Kambario dydis X ir Y, reikės Z pakuočių plytelių. Reikalinga pinigų suma E“
+*/
+
+/*
+8. Pirmos lietuviškos litų monetos išėjo 1925 metais. Tai buvo 5, 2 ir 1 lito vertės sidabrinės monetos. Duotą pinigų sumą 
+n (litais) pakeiskite nurodytomis monetomis, panaudojant kuo mažiau monetų.  
+Atsakymo galimas pvz: „Norint gauti n sumą litais, mums reikės n1 monetų po 5 lt, n2 monetų po 2 litus ir n3 monetų po 1 litą. 
+Viso monetų N skaičius“.
+*/
